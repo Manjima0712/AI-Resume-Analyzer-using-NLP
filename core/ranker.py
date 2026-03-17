c58d81501aacb4ba780c285939dfd3e38bf9ff8c
ManjimaSB <manjimasb@gmail.com>
2026-03-17 15:36:40 +0530

    deleted files

--- core/ranker.py
+++
@@ -1,5 +0,0 @@
-class Ranker:
-    @staticmethod
-    def rank_resumes(results):
-        """Rank a list of resume result dicts by their shortlist_score (descending)."""
-        return sorted(results, key=lambda x: x.get('shortlist_score', 0), reverse=True)
